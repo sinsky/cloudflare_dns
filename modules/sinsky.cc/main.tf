513ec15651845ed7ea1ae4719bc48bd0c9dd4d34
sinsky <60662802+sinsky@users.noreply.github.com>
2025-03-10 00:41:45 +0900

    feat(terraform): update DNS records to use YAML configuration for better maintainability and scalability refactor(terraform): change hardcoded DNS record values to dynamic values from local.dns_records for improved flexibility

--- modules/sinsky.cc/main.tf
+++ modules/sinsky.cc/main.tf
@@ -7,13 +7,19 @@ terraform {
   }
 }
 
+locals {
+  dns_records = yamldecode(file("${path.module}/dns_records.yaml"))
+}
 
-resource "cloudflare_dns_record" "www" {
-  name    = "www"
-  ttl     = 3600
-  type    = "A"
-  zone_id = var.cloudflare_zone_id
-  content = "100.109.137.3"
-  proxied = false
-  comment = "terraform Demo2"
+resource "cloudflare_dns_record" "records" {
+  for_each = local.dns_records
+
+  zone_id  = var.cloudflare_zone_id
+  name     = each.key
+  type     = each.value.type
+  content    = each.value.content
+  ttl      = lookup(each.value, "ttl", 3600)
+  proxied  = lookup(each.value, "proxied", false)
+  priority = lookup(each.value, "priority", null)
+  comment  = lookup(each.value, "comment", null)
 }
